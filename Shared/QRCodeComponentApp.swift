94abe61797e673503b13ad4ef0604c8272859db8
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-23 11:18:38 +0900

    Initial Commit

---
+++ Shared/QRCodeComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
