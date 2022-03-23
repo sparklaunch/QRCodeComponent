1a8813b212fc334a88b554f4e0261f314a84679d
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-23 11:24:37 +0900

    Add the font.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
+            .font(.custom("Outfit", size: 32))
             .padding()
     }
 }
