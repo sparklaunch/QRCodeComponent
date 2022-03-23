2300e0de347e0f52a3565fbc962042b3142abe85
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-23 11:48:18 +0900

    Add the body.

--- Shared/Components/Card.swift
+++ Shared/Components/Card.swift
@@ -24,12 +24,24 @@ struct Card: View {
                     .foregroundColor(Color("HeaderColor"))
                     .fixedSize(horizontal: false, vertical: true)
                     .multilineTextAlignment(.center)
+                    .padding(.horizontal, 5)
+                Spacer()
+                    .frame(height: 20)
+                Text("Scan the QR code to visit Frontend Mentor and take your coding skills to the next level")
+                    .font(.custom("Outfit", size: 18))
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color("BodyColor"))
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 5)
+                Spacer()
+                    .frame(height: 10)
             }
             .padding(20)
         }
         .clipShape(RoundedRectangle(cornerRadius: 15))
         .padding(.horizontal, 30)
         .scaledToFit()
-        .shadow(radius: 5)
+        .shadow(radius: 10)
     }
 }
